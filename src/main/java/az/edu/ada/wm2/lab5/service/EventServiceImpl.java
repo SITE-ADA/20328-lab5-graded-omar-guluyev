bec852d701cde4189b641634f1641b4cbbb390ca
omar-guluyev <150781152+omar-guluyev@users.noreply.github.com>
2026-02-26 10:55:33 +0400

    Implement getEventsByTag method in EventServiceImpl

--- src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
+++ src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
@@ -108,4 +108,19 @@ public class EventServiceImpl implements EventService {
         return null;
     }
 
+    @Override
+    public List<Event> getEventsByTag(String tag) {
+        if (tag == null || tag.isBlank()) {
+            return List.of();
+        }
+
+        return eventRepository.findAll()
+                .stream()
+                .filter(event -> event.getTags() != null &&
+                        event.getTags().stream()
+                                .anyMatch(t -> t.equalsIgnoreCase(tag)))
+                .collect(Collectors.toList());
+    }
+
+
 }
